573c2cb5faeb03a33393653d9519daa0dc77a65b
mlgmontage <kettleofrassel1@gmail.com>
2021-02-22 15:54:45 +0500

    words list & creation

--- api/routes/words.js
+++ api/routes/words.js
@@ -1,15 +1,24 @@
 const { Router } = require("express");
 const router = Router();
+const db = require("monk")("localhost/WordClipper");
+const words = db.get("words");
+words.createIndex("word");
 
 // List of words
 router.get("/", async (req, res) => {
-  res.json([{ word: "abundant", description: "plentyfull" }]);
+  const wordsList = await words.find({});
+
+  res.json(wordsList);
 });
 
-router.post("/create", (req, res) => {
-  res.json({
-    message: "created",
-  });
+router.post("/create", async (req, res) => {
+  const body = req.body;
+  if (body.word != undefined && body.description != undefined) {
+    const created = await words.insert(body);
+    res.json(created);
+  } else {
+    res.status(502).json({ message: "Bad request" });
+  }
 });
 
 module.exports = router;
